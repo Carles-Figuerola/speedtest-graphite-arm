d35e9da5eea65aa9c7c48f9e3ee7259ab2632d8d
Carles Figuerola <carles@figuerola.info>
2021-05-27 11:48:38 -0500

    Add timeout, add debug

--- test_speed.sh
+++ test_speed.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 GRAPHITE_HOST=${1:-localhost}
 GRAPHITE_PORT=${2:-2003}
@@ -6,7 +6,13 @@ GRAPHITE_PREFIX=${3:-internet}
 TIME=`date +%s`
 file="/tmp/speedtest"
 
-speedtest --format=json | tee $file
+timeout 300 speedtest --format=json | tee $file
+
+if [ ! -s "$file" ]; then
+  echo "File is empty"
+  exit 1
+fi
+
 ping=$(cat $file | jq .ping.latency)
 download=$(cat $file | jq .download.bandwidth)
 upload=$(cat $file | jq .upload.bandwidth)
